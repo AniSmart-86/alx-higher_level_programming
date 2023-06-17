ad9d78f1f2dcc5859234ed25316de12dd9151b48
AniSmart-86 <anismart124@gmail.com>
2023-06-17 01:58:54 +0000

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+
+nary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
---
+++ 0x04-python-more_data_structures/main.h
@@ -0,0 +1,13 @@
+quare_matrix_simple(matrix=[]):`                                                                    |
+| `1-search_replace.py`          | `def search_replace(my_list, search, replace):`                                                           |
+| `2-uniq_add.py`                | `def uniq_add(my_list=[]):`                                                                               |
+| `3-common_elements.py`         | `def common_elements(set_1, set_2):`                                                                      |
+| `4-only_diff_elements.py`      | `def only_diff_elements(set_1, set_2):
+| `5-number_keys.py`            def number_keys(a_dictionary):`                                                                                  def print_sorted_dictionary(a_dictionary):`                                                              |
+def update_dictionary(a_dictionary, key, value):`                                                        |
+def simple_delete(a_dictionary, key=""):`                                                                |
+def multiply_by_2(a_dictionary):`                                                                        |
+def best_score(a_dictionary):`                                                                           |
+def mutiply_list_map(my_list=[], number=0):`                                                             |
+def roman_to_int(roman_string):`                                                                         |
+def weight_average(my_list=[]):
