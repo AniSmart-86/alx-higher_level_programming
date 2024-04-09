8128d8a76a7507ec4df462de5ac0932587c18c67
AniSmart-86 <anismart124@mail.com>
2024-04-09 07:05:14 -0700

    js warm up

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const C = 'C is fun';
+const python = 'python is cool';
+const javascript = 'Javascript is amazing';
+console.log('C');
+console.log('Python');
+console.log('javascript');
